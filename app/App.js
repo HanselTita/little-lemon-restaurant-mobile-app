f55f9a8390f4cd7b36b68be7aa1a2d6bee439a2a
Hansel <hanseltita@gmail.com>
2026-06-02 22:59:07 +0700

    feat: scrollable welcome Screen

--- app/App.js
+++ app/App.js
@@ -1,19 +1,24 @@
 import { View } from "react-native";
 
-import Footer from "../components/Fotter";
+import LittleLemonFooter from "../components/LittleLemonFooter";
 import LittleLemonHeader from "../components/LittleLemonHeader";
+import WelcomeScreen from "../components/WelcomeScreen";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#495E57",
-        justifyContent: "space-between",
-      }}
-    >
-      <LittleLemonHeader />
-      <Footer />
-    </View>
+    <>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#495E57",
+        }}
+      >
+        <LittleLemonHeader />
+        <WelcomeScreen />
+      </View>
+      <View style={{ backgroundColor: "#495E57" }}>
+        <LittleLemonFooter />
+      </View>
+    </>
   );
 }
--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -1,15 +1,16 @@
-import { View, Text } from 'react-native';
+import { Text, View } from "react-native";
 
 export default function LittleLemonHeader() {
   return (
-    <View style={{ flex: 0.15, backgroundColor: '#F4CE14' }}>
+    <View style={{ flex: 0.15, backgroundColor: "#F4CE14" }}>
       <Text
         style={{
           padding: 40,
           fontSize: 30,
-          color: 'black',
-          textAlign: 'center',
-        }}>
+          color: "black",
+          textAlign: "center",
+        }}
+      >
         Little Lemon
       </Text>
     </View>
