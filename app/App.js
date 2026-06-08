b50afe11c43c59cc180c62e74114c4a22562a672
Hansel <hanseltita@gmail.com>
2026-06-08 20:23:02 +0700

    feat: LoginScreen.js with keyboardType and secureTextEntry

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 import { StyleSheet, View } from "react-native";
 import LittleLemonFooter from "../components/LittleLemonFooter";
 import LittleLemonHeader from "../components/LittleLemonHeader";
+import LoginScreen from "../components/LoginScreen";
 import MenuItems from "../components/MenuItems";
 import WelcomeScreen from "../components/WelcomeScreen";
 
@@ -9,6 +10,7 @@ export default function App() {
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
+        <LoginScreen />
         <WelcomeScreen />
         <MenuItems />
       </View>
